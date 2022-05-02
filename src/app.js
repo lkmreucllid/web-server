1180fe08ee0087e39e04c0dc5424194a330dfea5
lkmreucllid <lkmreucllid01@gmail.com>
2022-05-02 06:48:15 +0530

    server is already setup on local host now working on to use http

---
+++ src/app.js
@@ -0,0 +1,81 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+
+const publicDirectory = path.join(__dirname, '../public')
+const veiwsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+
+app.set('view engine', 'hbs')
+app.set('views', veiwsPath)
+hbs.registerPartials(partialsPath)
+
+
+app.use(express.static(publicDirectory))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Home',
+        name: 'Lalit Kumar'
+    })
+})
+
+
+//Blank for the home page
+
+/*app.get('', (req, res) => {
+    res.send('<h1>Hello This is the new beginning</h1>')
+})
+
+app.get('/help', (req, res) => {
+    res.send('This is the Help Page')
+})
+*/
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Lalit Kumar',
+        message: 'This is the long paragraph for Help',
+        desc: 'In computer science, an AVL tree is a self-balancing binary search tree. It was the first such data structure to be invented. In an AVL tree, the heights of the two child subtrees of any node differ by at most one; if at any time they differ by more than one, rebalancing is done to restore this property.'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Us',
+        name: 'Lalit Kumar'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'Heavy rainfall detected around noon',
+        location: 'Philadelphia'
+    })
+})
+
+//This will match only if the url contains /help
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Lalit Kumar',
+        errorMessages: 'Help article not found'
+    })
+})
+
+//This will match all the url and show the data
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Lalit Kumar',
+        errorMessages: 'Page not Found'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up and running port: 3000')
+})
