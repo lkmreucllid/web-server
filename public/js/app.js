8eabde03aaff2b03d17a7b2839e007564de68a58
lkmreucllid <lkmreucllid01@gmail.com>
2022-05-04 10:42:58 +0530

    Changes done to modified about and weather forecast message

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,7 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageHolder = document.querySelector('#messageHolder')
 const messageDesc = document.querySelector('#messageDesc')
+    //const weatherIcon = document.querySelector('#weatherIcon')
 
 messageHolder.textContent = 'Loading...'
 
@@ -23,8 +24,9 @@ weatherForm.addEventListener('submit', (e) => {
 
                 }
                 //console.log(data)
+                //weatherIcon.src = data.forecast.current.weather_icons[0]
                 messageHolder.textContent = 'Searched Location: ' + data.location
-                messageDesc.textContent = data.forecast.current.weather_descriptions[0] + ' ' + data.forecast.current.temperature + ' "C'
+                messageDesc.textContent = data.forecast.current.weather_descriptions[0] + '. It is currently ' + data.forecast.current.temperature + ' degrees out, feels like ' + data.forecast.current.feelslike + '.Humidity is ' + data.forecast.current.humidity + '%. There is ' + data.forecast.current.precip + '% chance of rain'
             })
         })
         //console.log('Testing' + location)
